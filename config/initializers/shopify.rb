e8d58965b1a4904c97ec60edec0b1246fa58858d
Mike Schwab <mike.schwab@gmail.com>
2012-01-26 14:27:09 -0500

    removed shopify credentials

--- config/initializers/shopify.rb
+++
@@ -1,5 +0,0 @@
-# ShopifyAPI::Session.setup({:api_key => 'c080117f9c8a121425a43fee4268c2b3', :secret => '4effc257c18dc5617e2f76aaff8e99d0'})
-# 
-# session = ShopifyAPI::Session.new("wxyz-jewelry.myshopify.com")
-
-ShopifyAPI::Base.site = 'https://c080117f9c8a121425a43fee4268c2b3:e7806cfb2290c1e896071947a9f5d49f@wxyz-jewelry.myshopify.com/admin'
