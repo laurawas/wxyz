3038230d71e4128297ac3be4d2d78db143db9bda
Mike Schwab <mike.schwab@gmail.com>
2012-01-27 12:17:45 -0500

    click on lookbook to navigate

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,19 +28,21 @@ $(document).ready(function() {
 });
 
 var next_one;
+var shown;
 
 function show_next_image() {
 	next_one = $('.lookbook.veiled')[0];
 	$('.lookbook.shown').addClass('faded');
-	setTimeout("$('.lookbook.shown').removeClass('shown').addClass('elapsed');
+	setTimeout("$('.lookbook.shown').removeClass('shown').addClass('elapsed'); \
 	            $(next_one).addClass('shown').removeClass('veiled');", 400);
 	setTimeout("$(next_one).removeClass('faded');", 410);
 }
 
 function show_previous_image() {
 	next_one = $('.lookbook.elapsed').last();
-	$('.lookbook.shown').removeClass('shown').addClass('faded');
-	setTimeout("$('.lookbook.faded').addClass('veiled');
-	            $(next_one).addClass('shown').removeClass('veiled').removeClass('elapsed');", 400);
+	shown = $('.lookbook.shown')
+	$(shown).removeClass('shown').addClass('faded');
+	setTimeout("$(shown).addClass('veiled'); \
+	            $(next_one).addClass('shown').removeClass('elapsed');", 400);
 	setTimeout("$(next_one).removeClass('faded');", 410);
 }
