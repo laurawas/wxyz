1682737de0a959deb976c7d63634afd7bc518099
Mike Schwab <mike.schwab@gmail.com>
2012-01-26 22:21:56 -0500

    development shopify creds

---
+++ config/initializers/shopify_password.rb
@@ -0,0 +1,6 @@
+if Rails.env.development?
+  shopify_credentials = YAML::load(File.read(File.join(Rails.root, 'config', 'shopify.yml')))
+
+  SHOPIFY_USERNAME = shopify_credentials['username']
+  SHOPIFY_PASSWORD = shopify_credentials['password']
+end
