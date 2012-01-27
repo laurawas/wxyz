db94a224bf21e9c201a0f4f6ab593598cc265867
Mike Schwab <mike.schwab@gmail.com>
2012-01-27 17:23:13 -0500

    lookbook can wrap around

--- app/assets/javascripts/lookbook_fading.js
+++ app/assets/javascripts/lookbook_fading.js
@@ -1,19 +1,34 @@
 var next_one;
 var shown;
+var designation;
 
 function show_next_image() {
-	next_one = $('.lookbook.veiled')[0];
+	next_one = $('.lookbook.veiled').first();
+	designation = 'elapsed';
+	if (next_one.length == 0) {
+		next_one = $('.lookbook.elapsed').first();
+		$('.elapsed').addClass('veiled').removeClass('elapsed');
+		next_one.removeClass('veiled');
+		designation = 'veiled';
+	}
 	$('.lookbook.shown').addClass('faded');
-	setTimeout("$('.lookbook.shown').removeClass('shown').addClass('elapsed'); \
+	setTimeout("$('.lookbook.shown').removeClass('shown').addClass(designation); \
 	            $(next_one).addClass('shown').removeClass('veiled');", 400);
 	setTimeout("$(next_one).removeClass('faded');", 500);
 }
 
 function show_previous_image() {
 	next_one = $('.lookbook.elapsed').last();
+	designation = 'veiled';
+	if (next_one.length == 0) {
+		next_one = $('.lookbook.veiled').last();
+		$('.veiled').addClass('elapsed').removeClass('veiled');
+		next_one.removeClass('elapsed');
+		designation = 'elapsed';
+	}
 	shown = $('.lookbook.shown')
 	$(shown).removeClass('shown').addClass('faded');
-	setTimeout("$(shown).addClass('veiled'); \
-	            $(next_one).addClass('shown').removeClass('elapsed');", 400);
+	setTimeout("$(shown).addClass(designation); \
+	            $(next_one).addClass('shown').removeClass('veiled').removeClass('elapsed');", 400);
 	setTimeout("$(next_one).removeClass('faded');", 500);
 }
