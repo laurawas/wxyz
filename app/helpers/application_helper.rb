fb57fba4e9346ec3c986fb6e3894007281db2e72
Laura Wasserson <laurawas@gmail.com>
2012-01-27 22:04:17 -0500

    navigation title reorder

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
   def nav
     content_tag :nav, :id => 'sitenav' do
       link_to(image_tag('wxyz.png', :id => 'logo'), root_url) +
-      link_to('ABOUT', about_path) +
+      link_to('LOOKBOOK', lookbook_path) + 
       link_to('COLLECTION', collection_path) +
-      link_to('LOOKBOOK', lookbook_path) +
+      link_to('ABOUT', about_path) +
       link_to('CONTACT', new_wxyzer_path) +
       link_to('BLOG', 'http://wxyznyc.com')
     end
