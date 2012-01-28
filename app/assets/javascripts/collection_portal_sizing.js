412cb8e224898e6cf4e67da51b322cb1fb78eb1f
Mike Schwab <mike.schwab@gmail.com>
2012-01-28 15:21:58 -0500

    removed title images from collection page

--- app/assets/javascripts/collection_portal_sizing.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-	$('.portal img').mouseover(function() {
-		$(this).attr('src', $(this).attr('src').split('.')[0] + '-title.png');
-	});
-	$('.portal img').mouseout(function() {
-		$(this).attr('src', $(this).attr('src').split('-')[0] + '.png');
-	});
-})
