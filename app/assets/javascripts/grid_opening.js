b2b63a9c50b50558b626d315b35da70a83b1f3d6
Mike Schwab <mike.schwab@gmail.com>
2012-01-31 13:10:52 -0500

    grid opening stable and sophisticated - only timing issue remains

--- app/assets/javascripts/grid_opening.js
+++ app/assets/javascripts/grid_opening.js
@@ -4,21 +4,48 @@ $(function() {
 	}
 	
 	$(window).resize(function() {
-		console.log('resize');
 		open_grid();
 	});
 });
 
 function open_grid() {
+	$('.shopify_api_product').css({'border-right': '2px #B1D81D solid', 'border-bottom': '2px #B1D81D solid', 'border-top': 'none'});
 	var nexty;
+	var lasty = $('.shopify_api_product').last();
+	var rightmost = $('.shopify_api_product').last();
 	var collection_height = $('.collection_pieces').height();
+	$('.shopify_api_product').each(function(i, piece) {
+		if($(piece).position().left > $(rightmost).position().left) {
+			rightmost = piece;
+		}
+	});
+	var incomplete_row = false;
+	var relevant_offset = 250;
+	if($(rightmost).position().left > $(lasty).position().left) {
+		incomplete_row = true;
+		relevant_offset = 350;
+	}
+	
 	$('.shopify_api_product').each(function(i, piece) {
 		nexty = $(piece).next();
 		if(nexty && $(nexty).position().left < 50) {
 			$(piece).css('border-right', 'none');
 		}
-		if($(piece).position().top > (collection_height - 350)) {
+		if($(piece).position().top > (collection_height - relevant_offset)) {
 			$(piece).css('border-bottom', 'none');
 		}
 	});
+	
+	if(incomplete_row) {
+		var num_in_row = 0;
+		$('.shopify_api_product').each(function(i, piece) {
+			if($(piece).position().top == $(lasty).position().top) {
+				num_in_row++;
+				$(piece).css({'border-right': '2px #B1D81D solid', 'border-top': '2px #B1D81D solid'});
+			}
+		});
+		if(num_in_row != 1) {
+			$(lasty).css('border-right', 'none');
+		}
+	}
 }
