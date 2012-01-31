32248b9e8ec1f69b47419f123b6a345f6b7ffa09
Mike Schwab <mike.schwab@gmail.com>
2012-01-31 05:10:41 -0500

    grid opening code

--- app/assets/javascripts/grid_opening.js
+++ app/assets/javascripts/grid_opening.js
@@ -1,15 +1,24 @@
 $(function() {
 	if($('.shopify_api_product').length != 0) { // we are on the product gallery page
-		setTimeout("var nexty; \
-		var collection_height = $('.collection_pieces').height(); \
-		$('.shopify_api_product').each(function(i, piece) { \
-			nexty = $(piece).next(); \
-			if(nexty && $(nexty).position().left < 50) { \
-				$(piece).css('border-right', 'none'); \
-			} \
-			if($(piece).position().top > (collection_height - 350)) { \
-				$(piece).css('border-bottom', 'none'); \
-			} \
-		});", 1000);
+		setTimeout("open_grid();", 1000);
 	}
+	
+	$(window).resize(function() {
+		console.log('resize');
+		open_grid();
+	});
 });
+
+function open_grid() {
+	var nexty;
+	var collection_height = $('.collection_pieces').height();
+	$('.shopify_api_product').each(function(i, piece) {
+		nexty = $(piece).next();
+		if(nexty && $(nexty).position().left < 50) {
+			$(piece).css('border-right', 'none');
+		}
+		if($(piece).position().top > (collection_height - 350)) {
+			$(piece).css('border-bottom', 'none');
+		}
+	});
+}
