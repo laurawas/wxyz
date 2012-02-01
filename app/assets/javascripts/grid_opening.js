99cd8507c57c5e2b1b822d68cd3c412d6030adcf
Mike Schwab <mike.schwab@gmail.com>
2012-02-01 10:35:59 -0500

    grid opened every half second for 10 seconds

--- app/assets/javascripts/grid_opening.js
+++ app/assets/javascripts/grid_opening.js
@@ -1,8 +1,26 @@
 $(function() {
-	if(false && $('.shopify_api_product').length != 0) { // we are on the product gallery page
+	if($('.shopify_api_product').length != 0) { // we are on the product gallery page
+		setTimeout("open_grid();", 200);
 		setTimeout("open_grid();", 500);
 		setTimeout("open_grid();", 1000);
+		setTimeout("open_grid();", 1500);
+		setTimeout("open_grid();", 2000);
+		setTimeout("open_grid();", 2500);
+		setTimeout("open_grid();", 3000);
+		setTimeout("open_grid();", 3500);
+		setTimeout("open_grid();", 4000);
+		setTimeout("open_grid();", 4500);
 		setTimeout("open_grid();", 5000);
+		setTimeout("open_grid();", 5500);
+		setTimeout("open_grid();", 6000);
+		setTimeout("open_grid();", 6500);
+		setTimeout("open_grid();", 7000);
+		setTimeout("open_grid();", 7500);
+		setTimeout("open_grid();", 8000);
+		setTimeout("open_grid();", 8500);
+		setTimeout("open_grid();", 9000);
+		setTimeout("open_grid();", 9500);
+		setTimeout("open_grid();", 10000);
 	}
 	
 	$(window).resize(function() {
@@ -42,7 +60,7 @@ function open_grid() {
 		$('.shopify_api_product').each(function(i, piece) {
 			if($(piece).position().top == $(lasty).position().top) {
 				num_in_row++;
-				$(piece).css({'border-right': '2px #B1D81D solid', 'border-top': '2px #B1D81D solid'});
+				$(piece).css({'border-right': '2px #B1D81D solid'});
 			}
 		});
 		if(num_in_row != 1) {
