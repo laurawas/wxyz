d971e53ed5b9a64a1d5ab98ac5e3a67de585d4a3
Mike Schwab <mike.schwab@gmail.com>
2012-02-06 20:00:56 -0500

    fixed and slowed lookbook cycling

--- app/assets/javascripts/lookbook_fading.js
+++ app/assets/javascripts/lookbook_fading.js
@@ -8,13 +8,12 @@ function show_next_image() {
 	if (next_one.length == 0) {
 		next_one = $('.lookbook.elapsed').first();
 		$('.elapsed').addClass('veiled').removeClass('elapsed');
-		next_one.removeClass('veiled');
 		designation = 'veiled';
 	}
 	$('.lookbook.shown').addClass('faded');
 	setTimeout("$('.lookbook.shown').removeClass('shown').addClass(designation); \
-	            $(next_one).addClass('shown').removeClass('veiled');", 400);
-	setTimeout("$(next_one).removeClass('faded'); ensure_showing();", 500);
+	            $(next_one).addClass('shown').removeClass('veiled');", 1200);
+	setTimeout("$(next_one).removeClass('faded'); ensure_showing();", 1300);
 }
 
 function show_previous_image() {
@@ -23,14 +22,13 @@ function show_previous_image() {
 	if (next_one.length == 0) {
 		next_one = $('.lookbook.veiled').last();
 		$('.veiled').addClass('elapsed').removeClass('veiled');
-		next_one.removeClass('elapsed');
 		designation = 'elapsed';
 	}
 	shown = $('.lookbook.shown')
 	$(shown).removeClass('shown').addClass('faded');
 	setTimeout("$(shown).addClass(designation); \
-	            $(next_one).addClass('shown').removeClass('veiled').removeClass('elapsed');", 400);
-	setTimeout("$(next_one).removeClass('faded'); ensure_showing();", 500);
+	            $(next_one).addClass('shown').removeClass('veiled').removeClass('elapsed');", 1200);
+	setTimeout("$(next_one).removeClass('faded'); ensure_showing();", 1300);
 }
 
 function ensure_showing() {
